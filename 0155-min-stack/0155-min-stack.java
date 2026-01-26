5f720105b33d4ba64b2948879ffce350d6674176
Harinath E <harinathcreationz@gmail.com>
2026-01-26 17:29:17 +0530

    5 ms (83.58%) 47.2 MB (20.72%) 01-26-2026 0155-min-stack .java Medium

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,53 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack(){
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }  
+    
+    
+    public void push(int val) {
+        stack.push(val);
+        // for minStack condition
+        if( minStack.isEmpty() || val <= minStack.peek() ){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.isEmpty()) {
+        throw new RuntimeException("Stack is empty");
+        }
+    
+        int removedElem = stack.pop();
+        if( removedElem == minStack.peek()){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        if (stack.isEmpty()) {
+        throw new RuntimeException("Stack is empty");
+        }
+        return stack.peek();
+    }
+    
+
+    public int getMin() {
+        if (stack.isEmpty()) {
+        throw new RuntimeException("Stack is empty");
+        }
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
