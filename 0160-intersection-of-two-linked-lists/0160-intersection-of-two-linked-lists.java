2ff6bcd14e40e4ceffe11cb7241ffda0b1ded032
Harinath E <harinathcreationz@gmail.com>
2026-02-24 22:22:17 +0530

    10 ms (13.8%) 52.3 MB (98.98%) 02-24-2026 0160-intersection-of-two-linked-lists .java Easy

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        HashSet<ListNode> set = new HashSet<>();
+        
+        // Store all nodes of List A
+        ListNode temp = headA;
+        while (temp != null) {
+            set.add(temp);
+            temp = temp.next;
+        }
+        
+        // Traverse List B and check
+        temp = headB;
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        
+        return null;
+    }
+}
