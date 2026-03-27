bd8f058d265e9225fdee4343a83f73e697f19884
Harinath E <harinathcreationz@gmail.com>
2026-03-27 16:42:23 +0530

    6033 ms (33.86%) 21.3 MB (54.6%) 03-27-2026 0212-word-search-ii .py Hard

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,39 +1,31 @@
 class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+    def findWords(self, board, words):
         row, col = len(board), len(board[0])
         word_set = set(words)
-        prefix_set = set()
-
-        # Build all prefixes
+        prefix = set()
         for word in words:
-            for i in range(len(word)):
-                prefix_set.add(word[:i+1])
-
+            for j in range(len(word)):
+                prefix.add(word[:j+1])
         result = set()
-
-        def dfs(r, c, path):
-            if (r < 0 or r >= row or c < 0 or c >= col or board[r][c] == "#"):
+        
+        def dfs(r, c, ds):
+            if (r < 0 or r >= row or c < 0 or c >= col or board[r][c] == '#' ):
                 return
-
-            path += board[r][c]
-
-            if path not in prefix_set:
-                return
-
-            if path in word_set:
-                result.add(path)
-
+            ds += board[r][c]
+            
+            if ds not in prefix:
+                return  
+            if ds in word_set:
+                result.add(ds)
             temp = board[r][c]
-            board[r][c] = "#"  # Mark visited
-
-            for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
-                dfs(r+dr, c+dc, path)
-
-            board[r][c] = temp  # Unmark visited
-
-        # Start DFS from each cell
+            board[r][c] = '#'
+            
+            for dr, dc in [(1,0), (-1, 0), (0, 1), (0, -1)]:
+                dfs(r + dr , c + dc, ds)
+            # backtracking
+            board[r][c] = temp
+        
         for i in range(row):
             for j in range(col):
                 dfs(i, j, "")
-
         return list(result)
