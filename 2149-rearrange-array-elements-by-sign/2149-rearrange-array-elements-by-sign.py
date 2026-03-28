da4a8e4149ccba32045568e0523f6d357ad16d53
Harinath E <harinathcreationz@gmail.com>
2026-03-28 16:18:19 +0530

    59 ms (34.49%) 43.8 MB (26.75%) 03-28-2026 2149-rearrange-array-elements-by-sign .py Medium

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,29 +1,33 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         # Separate positive and negative numbers
-        pos = []
-        neg = []
+        pos, neg = [],[]
+        n = len(nums)
         
+        # compute the pos[], neg[]
         for num in nums:
-            if num < 0:
+            if( num < 0 ):
                 neg.append(num)
             else:
                 pos.append(num)
-
-        # Create result array
-        res = [0] * len(nums)
-
-        # Rearranging elements: positives at even indices, negatives at odd indices
-        i = 0
-        while i < len(pos) and i < len(neg):
-            res[2 * i] = pos[i]
-            res[2 * i + 1] = neg[i]
-            i += 1
-
-        # If there are remaining elements
-        if i < len(pos):
-            res[2 * i:] = pos[i:]
-        elif i < len(neg):
-            res[2 * i:] = neg[i:]
-
+        res = [0] * n
+        # main logic
+        if( len(pos) > len(neg) ):
+            for i in range(len(neg)):
+                res[i*2] = pos[i]
+                res[i*2+1] = neg[i]
+            
+            index = 2 * len(neg)
+            for i in range(len(pos) - len(neg)):
+                res[index] = pos[len(neg) + i]
+                index += 1
+        else:
+            for i in range(len(pos)):
+                res[i*2] = pos[i]
+                res[i*2+1] = neg[i]
+            
+            index = 2 * len(pos)
+            for i in range(len(neg) - len(pos)):
+                res[index] = neg[len(pos) + i]
+                index += 1
         return res
