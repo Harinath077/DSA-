4bbea8dd07d01d8f4f1d63b397d524a4a76a682c
Harinath E <harinathcreationz@gmail.com>
2026-04-07 20:28:05 +0530

    30 ms (72.28%) 20.9 MB (96.64%) 04-07-2026 0846-hand-of-straights .py Medium

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        count = Counter(hand)       # Step 1: Count frequency of each card
+        hand.sort()                 # Step 2: Sort the hand (important for greedy)
+
+        for num in hand:           # Step 3: Try to form groups
+            if count[num] > 0:
+                for i in range(num, num + groupSize):
+                    if count[i] == 0:
+                        return False
+                    count[i] -= 1
+        return True
+
+
