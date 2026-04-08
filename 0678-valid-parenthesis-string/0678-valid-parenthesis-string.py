2ac645951a61c4e4baa434d830d9438cf3a74c03
Harinath E <harinathcreationz@gmail.com>
2026-04-08 06:28:32 +0530

    7 ms (10.44%) 18.7 MB (99.98%) 04-08-2026 0678-valid-parenthesis-string .py Medium

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        if not s:
+            return False
+        @lru_cache(None)
+        def dfs(index, count):
+            if(count < 0):
+                return False
+            if(index == len(s)):
+                return count == 0
+            
+            # ch = '('
+            if s[index] == '(':
+                return dfs(index + 1, count + 1)
+            # ch == ')
+            elif s[index] == ')':
+                return dfs(index + 1, count - 1)
+            else:
+                # try three possible ways
+                return (
+                    dfs(index + 1, count + 1) or
+                    dfs(index + 1, count ) or
+                    dfs(index + 1,count - 1)
+                )
+            return False
+        return dfs(0, 0)
+    
