7876f0b8ac367ff113e60d2318c3262edb58ef22
Harinath E <harinathcreationz@gmail.com>
2026-04-22 05:12:10 +0530

    169 ms (32.18%) 71.7 MB (32.95%) 04-22-2026 0494-target-sum .py Medium

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,17 +1,20 @@
-from functools import lru_cache
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
 
         n = len(nums)
-        @lru_cache(None)
+        memo = {}
         def helper(ind, curSum):
             if ind < 0:
                 if curSum == target:
                     return 1
                 else:
                     return 0
-            
+            if( ind, curSum ) in memo:
+                return memo[(ind,curSum)]
+
             pluse = helper(ind - 1, curSum + nums[ind])
             minus = helper(ind - 1, curSum - nums[ind])
-            return pluse + minus
+            memo[(ind,curSum)] =  pluse + minus
+            return memo[(ind,curSum)]
         return helper(n-1,0)
