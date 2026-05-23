5e423f6d76b23c3be4bb825adf66546e7babfdaa
Harinath E <harinathcreationz@gmail.com>
2026-05-23 08:17:20 +0530

    0 ms (100%) 19.3 MB (16.1%) 05-23-2026 1752-check-if-array-is-sorted-and-rotated .py Easy

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,27 +1,21 @@
 class Solution:
     """
-    cases:
-        if already sorted ----> return True
-        else :
-            roated means check for changes and with that x value find sorted or not
+    The REAL Pattern
+
+    A valid sorted-rotated array has:
+
+    0 drops → already sorted
+    1 drop → rotated sorted
+    more than 1 drop → impossible
+
     """
     def check(self, nums: List[int]) -> bool:
-        sortedNums = sorted(nums)
-
-        if( nums == sortedNums):
-            return True
-
         n = len(nums)
-        pos = 0
-        for i in range(n-1):
-            if( nums[i] > nums[i+1]):
-                break
-            else:
-                pos += 1
-        
+
+        count = 0
         for i in range(n):
-            
-            if( nums[(i + pos+1) % n] != sortedNums[i]):
-                return False
-        return True
+            if( nums[i] > nums[(i+1) % n] ):
+                count += 1
+        
+        return count <= 1
         
