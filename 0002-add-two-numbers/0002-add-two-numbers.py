f5bf313afa437a30cf69d39477d8ce8502c4f817
Harinath E <harinathcreationz@gmail.com>
2026-06-03 22:40:05 +0530

    4 ms (44.79%) 19.3 MB (41.8%) 06-03-2026 0002-add-two-numbers .py Medium

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,25 +5,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        t1 ,t2 = l1, l2
-        dummyNode = ListNode(None)
-        curr = dummyNode
-        carry = 0
-        while t1 != None or t2 != None:
-            sum_ = carry
-            if t1 :
-                sum_ = sum_ + t1.val
-                t1 = t1.next
-            if t2:
-                sum_ = sum_ + t2.val
-                t2 = t2.next
-            newNode = ListNode(sum_ % 10)
-            carry = sum_ // 10
-            curr.next = newNode
-            curr = curr.next
-        if carry == 1:
-            newNode = ListNode(1)
-            curr.next = newNode
-        return dummyNode.next
+        temp = l1
+        arr1 = []
+        while(temp != None):
+            arr1.append(temp.val)
+            temp = temp.next
+        temp = l2
+        arr2 = []
+        while(temp != None):
+            arr2.append(temp.val)
+            temp = temp.next
+            
+        n1 = int("".join(map(str,arr1[::-1])))
+        n2 = int("".join(map(str,arr2[::-1])))
+        # sum 
+        sum_ = list(map(int,str(n1+n2)))
+        sum_.reverse()
+        # Create a new linked list from the sum
+        head = ListNode(sum_[0])
+        temp = head
+        for i in range(1, len(sum_)):
+            temp.next = ListNode(sum_[i])
+            temp = temp.next
+
+        return head
+
 
         
