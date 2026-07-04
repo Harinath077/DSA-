3df086a344b5ad1e1ad10f5eef7ec4993819aa08
Harinath E <harinathcreationz@gmail.com>
2026-07-04 17:25:17 +0530

    187 ms (9.07%) 57.6 MB (15.73%) 07-04-2026 3951-minimum-energy-to-maintain-brightness .py Medium

---
+++ 3951-minimum-energy-to-maintain-brightness/3951-minimum-energy-to-maintain-brightness.py
@@ -0,0 +1,32 @@
+class Solution:
+    
+    def minEnergy(self, n: int, brightness: int, intervals: list[list[int]]) -> int:
+        def merge_intervals( intervals ):
+            new_intervals = []
+            intervals.sort()
+
+            for interval in intervals:
+                start, end = interval
+
+                # if new empty or not overlapping
+                if( not new_intervals or start > new_intervals[-1][1] ):
+                    new_intervals.append([start, end])
+                else:
+                    # overlapping
+                    new_intervals[-1][1] = max(new_intervals[-1][1], end)
+
+            return new_intervals
+                
+
+      
+        intervals = merge_intervals(intervals)
+        
+        active_time = 0
+        for start, end in intervals:
+            active_time += end - start + 1
+
+        minimum_bulbs = ceil( brightness / 3)
+
+        return minimum_bulbs * active_time
+            
+            
