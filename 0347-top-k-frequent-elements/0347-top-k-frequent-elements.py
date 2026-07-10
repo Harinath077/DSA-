c801feebc33590a7a618b2a4dd7e7ae05d913799
Harinath E <harinathcreationz@gmail.com>
2026-07-10 13:24:46 +0530

    7 ms (51.95%) 23.1 MB (31.49%) 07-10-2026 0347-top-k-frequent-elements .py Medium

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,24 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = {}
+        max_heap = []
+        ans = []
         for num in nums:
             if num not in freq:
                 freq[num] = 0
             freq[num] += 1
 
-        sorted_freq = dict(
-            sorted(freq.items(), 
-                    key = lambda item : item[1], 
-                    reverse = True)
-            )
+        for key, val in freq.items():
+            heapq.heappush(max_heap, (-val, key))
+        
+        for _ in range(k):
+            ans.append( heapq.heappop(max_heap)[1])
+        
+        return ans
 
-        res = []
-        i = 0
-        for key in sorted_freq.keys():
-            if( i == k):
-                break
-            res.append(key)
-            i += 1
-
-        return res
 
