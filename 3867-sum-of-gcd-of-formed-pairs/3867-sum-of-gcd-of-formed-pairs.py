ef4f27d4254cbaf287d796ef3bdcfe0cf9da76ac
Harinath E <harinathcreationz@gmail.com>
2026-07-16 22:33:23 +0530

    470 ms (20.36%) 33.3 MB (97.6%) 07-16-2026 3867-sum-of-gcd-of-formed-pairs .py Medium

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def gcdSum(self, nums: list[int]) -> int:
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        n = len(nums)
+
+        prefix_max = [0] * n
+        prefix_gcd = [0] * n
+
+        prefix_max[0] = nums[0]
+
+        for i in range(1, n):
+            prefix_max[i] = max(prefix_max[i - 1], nums[i])
+
+        for i in range(n):
+            prefix_gcd[i] = gcd(prefix_max[i], nums[i])
+
+        prefix_gcd.sort()
+
+        res = 0
+
+        for i in range(n // 2):
+            res += gcd(prefix_gcd[i], prefix_gcd[n - 1 - i])
+
+        return res
