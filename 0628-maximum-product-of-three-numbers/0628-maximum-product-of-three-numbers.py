e72ec26e8beb6d1986c5bc9273f0e9c6c0b1d0b9
Harinath E <harinathcreationz@gmail.com>
2026-07-26 08:47:13 +0530

    9 ms (88.91%) 20.2 MB (78.13%) 07-26-2026 0628-maximum-product-of-three-numbers .py Easy

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -7,15 +7,31 @@ class Solution:
     --------> 2 -ve nums give +ve nums ( that is the logic)
     """
     def maximumProduct(self, nums: List[int]) -> int:
-        nums.sort()
-        
-        max1 = nums[-1]
-        max2 = nums[-2]
-        max3 = nums[-3]
-        
-        min1 = nums[0]
-        min2 = nums[1]
+        max1 = float('-inf')
+        max2 = float('-inf')
+        max3 = float('-inf')
 
+        min1 = float('inf')
+        min2 = float('inf')
+
+        for num in nums:
+
+            if num > max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max3 = max2
+                max2 = num
+            elif num > max3:
+                max3 = num
+            
+            # negative handling
+            if num < min1:
+                min2 = min1
+                min1 = num
+            elif num < min2:
+                min2 = num
         return max(
             max1 * max2 * max3,
             min1 * min2 * max1
