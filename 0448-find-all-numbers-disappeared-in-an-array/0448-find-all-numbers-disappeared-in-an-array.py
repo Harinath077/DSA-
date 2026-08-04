9a7ce43ac2bc6338af6b386f1653df774ee1551f
Harinath E <harinathcreationz@gmail.com>
2026-08-05 04:34:53 +0530

    47 ms (20.02%) 30.9 MB (60.15%) 08-05-2026 0448-find-all-numbers-disappeared-in-an-array .py Easy

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mapp = {}
+        result = []
+        for i in range(n):
+            mapp[nums[i]] = mapp.get(nums[i], 0) + 1
+        
+        for i in range(1, n + 1):
+            if mapp.get(i, 0) == 0:
+                result.append(i)
+        return result
