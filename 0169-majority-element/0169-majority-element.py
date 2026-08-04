9f9da4370d4a8bd0eaef5b44f6f89e5029beb6ba
Harinath E <harinathcreationz@gmail.com>
2026-08-04 17:53:25 +0530

    8 ms (40.97%) 21.2 MB (18.75%) 08-04-2026 0169-majority-element .py Easy

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        me = nums[0]
+        count = 1
+
+        for i in range(1, n):
+            if me == nums[i]:
+                count += 1
+            else:
+                count -= 1
+            if count == 0:
+                me = nums[i]
+                count = 1
+        
+        return me
+        
