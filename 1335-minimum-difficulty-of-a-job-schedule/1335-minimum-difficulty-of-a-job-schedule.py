3e4eebfcb5165ea8b23bc9cf8d424d051e606567
Harinath E <harinathcreationz@gmail.com>
2026-08-13 22:33:56 +0530

    272 ms (86.28%) 19.5 MB (71.97%) 08-13-2026 1335-minimum-difficulty-of-a-job-schedule .py Hard

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,35 @@
+class Solution:
+    """  
+    partition approach : front partition 
+    """
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        
+        n = len(jobDifficulty)
+
+        # edge case
+        if n < d:
+            return -1
+
+        dp = [[float('inf')] * (d+1) for _ in range(n+1)]
+
+        # base case 
+        max_ = 0
+        for index in range(n-1, -1, -1):
+            max_ = max( max_, jobDifficulty[index])
+            dp[index][1] = max_
+
+        for index in range( n-1, -1, -1):
+            for balanceCuts in range(2, d+1):
+
+                maxElement = -1
+                min_ = float('inf')
+
+                for j in range(index, n):
+
+                    maxElement = max( jobDifficulty[j], maxElement )
+                    ans = maxElement + dp[j + 1][balanceCuts-1]
+                    min_ = min(ans, min_)
+
+                dp[index][balanceCuts] = min_
+        
+        return dp[0][d]
