a9ed22cdb93e85aaf78670030d27275f97cb8d76
Harinath E <harinathcreationz@gmail.com>
2026-08-15 15:34:23 +0530

    21 ms (87.9%) 33.1 MB (88.71%) 08-15-2026 3702-longest-subsequence-with-non-zero-bitwise-xor .py Medium

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -1,6 +1,6 @@
 class Solution:
     """  
-    Case 1: All elements are 0 --- return 0
+    Case 1: All elements are 0 ---> return 0
     Case 2: The total XOR sum is non-zero ----> so longest one Arrays total length itself So return n
     Case 3: The total XOR sum is zero ----> means that xor is cancel outed the all element so removing one element will help in not cancle out the all element So return n-1
     """
