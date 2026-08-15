ec0b5ecc7c6bdd6284f7cdd612331197c01afacc
Harinath E <harinathcreationz@gmail.com>
2026-08-15 15:34:05 +0530

    12 ms (97.58%) 33.3 MB (49.19%) 08-15-2026 3702-longest-subsequence-with-non-zero-bitwise-xor .py Medium

---
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -0,0 +1,25 @@
+class Solution:
+    """  
+    Case 1: All elements are 0 --- return 0
+    Case 2: The total XOR sum is non-zero ----> so longest one Arrays total length itself So return n
+    Case 3: The total XOR sum is zero ----> means that xor is cancel outed the all element so removing one element will help in not cancle out the all element So return n-1
+    """
+    def longestSubsequence(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # case 1
+        if all(num == 0 for num in nums):
+            return 0
+        
+        # compute totalXor 
+        totalXor = 0
+        for num in nums:
+            totalXor ^= num
+        
+        # case 2
+
+        if totalXor > 0:
+            return n
+        
+        # case 3
+        return n-1
