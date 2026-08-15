6c1988066a945bef23f7beff6e3c9840e09799de
Harinath E <harinathcreationz@gmail.com>
2026-08-15 20:16:20 +0530

    24 ms (11.48%) 19.4 MB (5.42%) 08-15-2026 3090-maximum-length-substring-with-two-occurrences .py Easy

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -1,33 +1,13 @@
 class Solution:
-    # sliding window
-    # expanad : untill reach the 2 occurances
-    # shrink : if reached the 2 occurances
-
     def maximumLengthSubstring(self, s: str) -> int:
         n = len(s)
-        if n == 1:
-            return 1
+        longest = -1
+        for i in range(n):
+            freq = {}
+            for j in range(i, n):
+                freq[s[j]] = freq.get(s[j], 0) + 1
+                if freq[s[j]] > 2:
+                    break
+                longest = max( longest, j - i + 1)
         
-        left = 0
-        right = 0
-        mapp = defaultdict(int)
-        maxLen = -1
-        while right < n:
-
-            mapp[s[right]] = mapp.get(s[right], 0) + 1
-
-            while mapp[s[right]] > 2:
-                mapp[s[left]] -= 1
-
-                if mapp[s[left]] == 0:
-                    del mapp[s[left]]
-                left += 1
-            
-            if mapp[s[right]] <= 2:
-                maxLen = max( maxLen, right - left + 1)
-                
-            right += 1
-        return maxLen
-
-                
-
+        return longest
