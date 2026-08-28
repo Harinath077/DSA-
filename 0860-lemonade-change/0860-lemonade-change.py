981abf891cefbde16dc473b59e4c646d4be8daf2
Harinath E <harinathcreationz@gmail.com>
2026-08-28 12:40:13 +0530

    7 ms (26.09%) 23.4 MB (39.12%) 08-28-2026 0860-lemonade-change .py Easy

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,26 +1,32 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
+        
         five = 0
         ten = 0
-        
+        twenty = 0
 
-        for bill in bills:
-            if bill == 5:
+        for coin in bills:
+
+            if coin == 5:
                 five += 1
-            elif bill == 10:
-                
+
+            elif( coin == 10 ):
+
                 if five == 0:
                     return False
+                
                 five -= 1
                 ten += 1
+            
             else:
-                
+                twenty += 1
                 if ten > 0 and five > 0:
-                    ten -= 1
                     five -= 1
+                    ten -= 1
                 elif five >= 3:
                     five -= 3
+                
                 else:
                     return False
+        
         return True
-                
