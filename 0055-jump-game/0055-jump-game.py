3c8a9354c87e11274a661c173c3fc67bdd754635
Harinath E <harinathcreationz@gmail.com>
2026-08-28 15:11:08 +0530

    11 ms (91.54%) 20.3 MB (45.66%) 08-28-2026 0055-jump-game .py Medium

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+        farthest = 0
+
+        for i in range(n):
+
+            if i > farthest:
+                return False
+            
+            farthest = max(farthest, i + nums[i])
+
+            # optimal exit
+            if farthest > n-1:
+                return True
+        
+        return True
