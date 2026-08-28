e30ed5db15d5dabfd19d42dfbf03ac7928e06232
Harinath E <harinathcreationz@gmail.com>
2026-08-28 16:23:00 +0530

    3 ms (88.85%) 20.1 MB (65.53%) 08-28-2026 0045-jump-game-ii .py Medium

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,36 +1,20 @@
 class Solution:
-    """  
-    dfs(i) --> retun the minimum number of steps to reach last index from i
-    """
     def jump(self, nums: List[int]) -> int:
-        
-        def dfs(index):
-
-            if index >= n-1:
-                return 0
-            
-            if memo[index] != -1:
-                return memo[index]
-            
-            min_ = float('inf')
-            jumps = nums[index]
-            for j in range(1, jumps + 1):
-                min_ = min( min_, 1 + dfs(index + j))
-
-            memo[index] = min_
-            return memo[index]
-        
         n = len(nums)
-        dp = [float('inf')] * n
+        jumps = 0
+        l = 0
+        r = 0
 
-        dp[n-1] = 0
+        while r < n-1:
 
-        for index in range(n-2, -1, -1):
-            min_ = float('inf')
-            jumps = nums[index]
-            for j in range(1, min(jumps, n - index - 1)+1):
-                min_ = min( min_, 1 + dp[index + j])
+            farthest = 0
 
-            dp[index] = min_
-            
-        return dp[0]
+            for index in range(l, r+1):
+
+                farthest = max( farthest, index + nums[index])
+
+            l = r + 1
+            r = farthest
+            jumps += 1
+
+        return jumps
