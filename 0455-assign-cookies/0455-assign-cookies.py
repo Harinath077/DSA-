6141eacc9736442a83b305ce74a0136f0a4c0553
Harinath E <harinathcreationz@gmail.com>
2026-08-28 12:27:38 +0530

    19 ms (79%) 21.9 MB (88.29%) 08-28-2026 0455-assign-cookies .py Easy

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        
+        g.sort()
+        s.sort()
+        
+        n = len(g)
+        m = len(s)
+        i = 0
+        j = 0
+
+        while( i < n and j < m ):
+
+            if s[j] >= g[i]:
+                i += 1
+            
+            j += 1
+
+        return i
