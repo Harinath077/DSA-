8e14a45248f0196c6248adf0cbc406c3cfa411e5
Harinath E <harinathcreationz@gmail.com>
2026-08-29 17:18:56 +0530

    75 ms (5.1%) 25.6 MB (14.9%) 08-29-2026 0135-candy .py Hard

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,32 +1,31 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+
         n = len(ratings)
 
-        left = [0] * n
-        right = [0] * n
+        left = [1] * n
+        right = [1] * n
 
-        # default value intialization
-        left[0] = 1
-        right[n-1] = 1
-
-        # hadling left increasing slop
-        for i in range(1, n):
-            if( ratings[i] > ratings[i-1] ):
-                left[i] += left[i-1] + 1
-            else:
-                left[i] = 1
+        for i in range(1,n):
+            if ratings[i] > ratings[i-1]:
+                left[i] = left[i-1] + 1
         
-        # handling for right increasing slop
         for i in range(n-2, -1, -1):
-            if( ratings[i] > ratings[i+1] ):
-                right[i] += right[i+1] + 1
-            else:
-                right[i] = 1
+            if ratings[i] > ratings[i+1]:
+                right[i] = right[i+1] + 1
         
-        # find the answering by combaining
-        noOfCandies = 0
+        # main logic
+
+        res = 0
+
         for i in range(n):
-            noOfCandies += max(left[i], right[i])
+            res += max( left[i], right[i] )
         
-        return noOfCandies
-            
+        return res
+
+
+
+
+
+
+        
