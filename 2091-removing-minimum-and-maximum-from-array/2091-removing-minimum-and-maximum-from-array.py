cd03fa1feae7fc5ae7833d55cd3dfaa3220e5546
Harinath E <harinathcreationz@gmail.com>
2026-08-30 13:14:20 +0530

    15 ms (81.27%) 33.3 MB (96.98%) 08-30-2026 2091-removing-minimum-and-maximum-from-array .py Medium

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,59 +1,20 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-
-        def search(left, right, target):
-            skips = 0
-
-            for i in range(left, right):
-                skips += 1
-
-                if nums[i] == target:
-                    return skips
-
-        def searchB(right, left, target):
-            skips = 0
-
-            for i in range(right, left, -1):
-                skips += 1
-
-                if nums[i] == target:
-                    return skips
-
         n = len(nums)
 
-        min_ = min(nums)
-        max_ = max(nums)
+        minIndex = nums.index(min(nums))
+        maxIndex = nums.index(max(nums))
 
-        minIndex = nums.index(min_)
-        maxIndex = nums.index(max_)
+        # from front side
+        front = max(minIndex, maxIndex) + 1
 
-        # 1. Delete only from FRONT
-        cnt1 = search(0, n, min_)
-        cnt2 = search(0, n, max_)
-        delt1 = max(cnt1, cnt2)
+        # form back side
+        back = n - min(minIndex, maxIndex)
 
-        # 2. Delete only from BACK
-        cntB1 = searchB(n - 1, -1, min_)
-        cntB2 = searchB(n - 1, -1, max_)
-        delt2 = max(cntB1, cntB2)
-
-        
-        # 3. Delete from FRONT and BACK
-        # Option A:
-        # min from front + max from back
-        option1 = (
-            search(0, n, min_) +
-            searchB(n - 1, -1, max_)
+        # from both side
+        both = min(
+            (minIndex + 1) + (n - maxIndex),
+            (maxIndex + 1) + (n - minIndex)
         )
 
-        # Option B:
-        # max from front + min from back
-        option2 = (
-            search(0, n, max_) +
-            searchB(n - 1, -1, min_)
-        )
-
-        delt3 = min(option1, option2)
-
-
-        return min(delt1, delt2, delt3)
+        return min(front, back, both)
