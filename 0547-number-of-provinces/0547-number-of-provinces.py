1cc2610537ee84da8e9dd9bd9dff59647be1ede1
Harinath E <harinathcreationz@gmail.com>
2026-09-02 13:51:31 +0530

    3 ms (86.29%) 21.6 MB (20.97%) 09-02-2026 0547-number-of-provinces .py Medium

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,28 +4,20 @@ class Solution:
         def dfs(node):
             visited[node] = True
 
-            for neighbour in adjList[node]:
-                if not visited[neighbour]:
+            for neighbour in range(V):
+                if isConnected[node][neighbour] == 1 and not visited[neighbour]:
                     dfs(neighbour)
+            
 
         V = len(isConnected)
-        # build the graph
-        adjList = [[] for _ in range(V)]
-
-        for i in range(V):
-            for j in range(V):
-                if isConnected[i][j] == 1:
-                    adjList[i].append(j)
-                    adjList[j].append(i)
         
+
         visited = [False] * V
         provinces = 0
-        
+
         for node in range(V):
             if not visited[node]:
                 provinces += 1
                 dfs(node)
-        
+
         return provinces
-
-
