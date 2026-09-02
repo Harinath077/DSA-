fc72c740b939e52f003be3266e2737feab2b0b23
Harinath E <harinathcreationz@gmail.com>
2026-09-02 13:49:55 +0530

    10 ms (21.15%) 21.4 MB (37.6%) 09-02-2026 0547-number-of-provinces .py Medium

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,27 +1,31 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
         def dfs(node):
             visited[node] = True
-            for neighbour in adj[node]:
-                if( not visited[neighbour]):
+
+            for neighbour in adjList[node]:
+                if not visited[neighbour]:
                     dfs(neighbour)
 
-        def buildAdjList():
-            addList = [[] for _ in range(V)]
-            for i in range(V):
-                for j in range(V):
-                    if( isConnected[i][j] == 1):
-                        addList[i].append(j)
-                        addList[j].append(i)
-            return addList
-
         V = len(isConnected)
-        visited = [False] * V
-        adj = buildAdjList()
-        provinces = 0 
+        # build the graph
+        adjList = [[] for _ in range(V)]
 
+        for i in range(V):
+            for j in range(V):
+                if isConnected[i][j] == 1:
+                    adjList[i].append(j)
+                    adjList[j].append(i)
+        
+        visited = [False] * V
+        provinces = 0
+        
         for node in range(V):
-            if( not visited[node] ):
+            if not visited[node]:
                 provinces += 1
                 dfs(node)
+        
         return provinces
+
+
