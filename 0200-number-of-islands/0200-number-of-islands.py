91ebe29aec60e5c25b42fd36d79909f2daa640d5
Harinath E <harinathcreationz@gmail.com>
2026-09-02 14:27:19 +0530

    238 ms (72.77%) 22.2 MB (31.47%) 09-02-2026 0200-number-of-islands .py Medium

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(r, c):
-            queue = deque()
-            queue.append( (r, c) )
+        def dfs(r, c):
             visited[r][c] = True
 
-            while( queue ):
-                x, y = queue.popleft()
-                for dr, dc in dir:
-                    nr, nc = dr + x, dc + y
-                    if( 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == '1' and not visited[nr][nc] ):
-                        visited[nr][nc] = True
-                        queue.append( (nr, nc))
-                        bfs(nr, nc)
+            for dr, dc in directions:
+                nr = dr + r
+                nc = dc + c
+
+                if 0 <= nr < n and 0 <= nc < m and visited[nr][nc] == False and grid[nr][nc] == '1':
+                    dfs(nr, nc)
 
         n = len(grid)
         m = len(grid[0])
-        visited = [[False] * m for _ in range(n)]
+        visited = [[False] * m for _ in range(n) ]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         islands = 0
-
-        dir = [(0,-1),(-1, 0),(0, 1),(1, 0)]
 
         for r in range(n):
             for c in range(m):
-                if( not visited[r][c] and grid[r][c] == '1' ):
+                if visited[r][c] == False and grid[r][c] == '1':
                     islands += 1
-                    bfs(r, c)
+                    dfs(r, c)
+        
         return islands
-                
