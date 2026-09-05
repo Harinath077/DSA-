9d7374b452a815251e1a39e2a0386e557320cfd3
Harinath E <harinathcreationz@gmail.com>
2026-09-05 18:16:57 +0530

    8 ms (40.49%) 21 MB (56.06%) 09-05-2026 0740-delete-and-earn .py Medium

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,30 +1,5 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-    
-        def dfs(index):
-            # base case 
-            if index >= n:
-                return 0
-
-            if memo[index] != -1:
-                return memo[index]
-
-            points = arr[index] * freq[arr[index]]
-
-            # take 
-            nextIndex = index + 1
-
-            if nextIndex < n and arr[index]+1 == arr[nextIndex]:
-                nextIndex += 1
-            
-            take = points + dfs(nextIndex)
-
-            # not take
-            notTake = dfs(index + 1)
-
-            memo[index] = max(take, notTake)
-            return memo[index]
-
       
         freq = {}
         for num in nums:
@@ -32,5 +7,22 @@ class Solution:
         arr = sorted(freq.keys())
         n = len(arr)
 
-        memo = [-1] * n
-        return dfs(0)
+        dp = [0] * (n+1)
+
+        for index in range(n-1, -1, -1):
+            
+            points = arr[index] * freq[arr[index]]
+
+            # take 
+            nextIndex = index + 1
+
+            if nextIndex < n and arr[nextIndex] == arr[index] + 1:
+                nextIndex += 1
+
+            take = points + dp[nextIndex]
+
+            notTake = dp[index + 1]
+
+            dp[index] = max(take, notTake)
+
+        return dp[0]
