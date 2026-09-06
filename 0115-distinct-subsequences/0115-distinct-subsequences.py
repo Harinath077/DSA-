e7b176e866fb64e9ddce6837ce64cf16fda85599
Harinath E <harinathcreationz@gmail.com>
2026-09-06 12:05:23 +0530

    430 ms (44.1%) 75.6 MB (56.33%) 09-06-2026 0115-distinct-subsequences .py Hard

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,26 +1,6 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        def dfs(i, j):
-
-            # base case
-            if j == 0:
-                return 1
-                
-            if i == 0:
-                return 0
-
-            if memo[i][j] != -1:
-                return memo[i][j]
-
-            if s[i - 1] == t[j - 1]:
-
-                memo[i][j] = dfs(i - 1, j - 1) + dfs(i - 1, j)
-            else:
-                memo[i][j] = dfs(i - 1, j)
-            
-            return memo[i][j]
-        
         n = len(s)
         m = len(t)
         dp = [[0] * (m+1) for _ in range(n+1) ]
