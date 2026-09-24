67bbd39379373112f0c14e4cd9b4d60319c65eb4
Harinath E <harinathcreationz@gmail.com>
2026-09-24 13:45:20 +0530

    148 ms (31.21%) 68.7 MB (88.06%) 09-24-2026 0981-time-based-key-value-store .py Medium

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,26 +1,41 @@
 class TimeMap:
-
+    """  
+    {
+    "foo": [
+        [1, "bar1"],
+        [4, "bar2"]
+        ]
+    }
+    """
     def __init__(self):
         self.mapp = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.mapp:
-            self.mapp[key] = {}
-        self.mapp[key][timestamp] = value
+            self.mapp[key] = []
+        self.mapp[key].append( (timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
         
         if key not in self.mapp:
             return ""
         
-        timeStamps = sorted(self.mapp[key].keys())
-        # upperBound
-        index = bisect.bisect_right(timeStamps,timestamp)
-        if index-1 == -1:
-            return ""
-        time = timeStamps[index-1]
+        arr = self.mapp[key]
         
-        return self.mapp[key][time]
+        # floor
+        low = 0
+        high = len(arr)-1
+        ans = ""
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if arr[mid][0] <= timestamp:
+                ans = arr[mid][1]
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
