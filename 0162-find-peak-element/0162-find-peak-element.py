f3ecff7a81e11adf7d220077228e6eb012035af8
Harinath E <harinathcreationz@gmail.com>
2026-09-26 06:51:08 +0530

    0 ms (100%) 19.2 MB (81.03%) 09-26-2026 0162-find-peak-element .py Medium

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -9,7 +9,24 @@ class Solution:
         if nums[-1] > nums[-2]:
             return n-1
         
-        for i in range(1, n-1):
-            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-                return i
+        low = 0
+        high = n-1
+
+        while low <= high:
+
+            mid = low + (high - low) // 2
+
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+            
+            elif nums[mid] > nums[mid-1]:
+                # increasing order
+                low = mid + 1
+            elif nums[mid] > nums[mid+1]:
+                # decreasing order
+                high = mid - 1
+            else:
+                # edge case ---> [4, 5, 1, 6, 2]
+                #                    ^     ^
+                low = mid + 1
         
