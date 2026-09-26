6686fa6fbd58fdbf2f3116eb7e6b4f94e82bdd9d
Harinath E <harinathcreationz@gmail.com>
2026-09-26 16:14:53 +0530

    3868 ms (6.81%) 20 MB (9.67%) 09-26-2026 0410-split-array-largest-sum .py Hard

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,32 +1,14 @@
 class Solution:
     def splitArray(self, nums: list[int], k: int) -> int:
        
-        def helper(index, kLeft):
-
-            # base case
-            if kLeft == 1:
-                return sum(nums[index : ])
-
-            if dp[index][kLeft] != -1:
-                return dp[index][kLeft]
-
-            mini = float('inf')
-            maxSum = float('-inf')
-
-            for j in range(index, n-kLeft + 1):
-                currSum = sum(nums[index : j+1])
-                maxSum = max( currSum, helper(j + 1, kLeft - 1))
-                mini = min(mini, maxSum)
-            
-            dp[index][kLeft] = mini
-            return mini
-
         n = len(nums)
         dp = [[float('inf')] * (k+1) for _ in range(n+1)]
 
         # base case
-        for index in range(n):
-            dp[index][1] = sum( nums[index:] )
+        currSum = 0
+        for index in range(n-1, -1, -1):
+            currSum += nums[index]
+            dp[index][1] = currSum
 
         for index in range(n-1, -1, -1):
             for kLeft in range(2, k+1):
