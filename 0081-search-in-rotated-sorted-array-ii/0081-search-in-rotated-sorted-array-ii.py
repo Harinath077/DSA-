3651e1b1b23780d4bc869cb8aad1c03880283a91
Harinath E <harinathcreationz@gmail.com>
2026-09-26 06:09:19 +0530

    0 ms (100%) 19.6 MB (33.29%) 09-26-2026 0081-search-in-rotated-sorted-array-ii .py Medium

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,6 +1,29 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> bool:
-        for index, num in enumerate(nums):
-            if num == target:
+        n = len(nums)
+        low = 0
+        high = n-1
+
+        while low <= high:
+            mid = low + (high - low)//2
+
+            if nums[mid] == target:
                 return True
+            # handling duplicate
+            elif nums[low] == nums[mid] and nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+            # left is sorted
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target and target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                # right is sorted
+                if nums[mid] <= target and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return False
